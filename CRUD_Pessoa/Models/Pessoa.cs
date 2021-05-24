8858ffe987c5c1ed43fed01952eaca88ee4f9f07
Ana Freitas <ana_paula.10@hotmail.com>
2021-05-23 22:40:14 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD_Pessoa/Models/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_Pessoa.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+
+        [Display(Name = "Data de Nascimento")]
+        [DataType(DataType.Date, ErrorMessage = "Data em formato inválido")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime DataNascimento { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
---
+++ CRUD_Pessoa/Models/PessoasDB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_Pessoa.Models
+{
+    public class PessoasDB : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+    }
+}
